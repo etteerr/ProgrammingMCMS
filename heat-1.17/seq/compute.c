eabed3eebb1abcba00db516ccdc0d407bf22f81d
etteerr <e.j.diepgrond@gmail.com>
2017-02-15 08:05:22 +0100

    Changed allocation logic and map logic. Now map indices must be acces like it has no ghost rows

--- heat-1.17/seq/compute.c
+++ heat-1.17/seq/compute.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "../src/compute.h"
+#include "../src/output.h"
 
 #define sideCoef 0.58578643762
 
@@ -18,16 +19,16 @@ size_t hm_column = 0;
 
 //Getters and setters
 // Inline for performance
-inline double hm_get(size_t row, size_t column) {
-	return hm_prevData[hm_column * row + column];
+inline double hm_get(int row, int column) {
+	return hm_prevData[(int)(hm_column * row + column) + hm_column];
 }
 
-inline double hm_get_current(size_t row, size_t column) {
-	return hm_data[hm_column * row + column];
+inline double hm_get_current(signed long int  row, long int  column) {
+	return hm_data[(int)(hm_column * row + column) + hm_column];
 }
 
-inline void hm_set(size_t row, size_t column, double val) {
-	hm_data[hm_column * row + column] = val;
+inline void hm_set(long int row, long int column, double val) {
+	*(hm_data + (int)(hm_column * row + column) + hm_column) = val;
 }
 
 inline double hm_get_coeff(size_t row, size_t column) {
@@ -55,9 +56,9 @@ void do_compute(const struct parameters* p, struct results *r)
 	double cdiag, cnext;
 
 	// Calculate the inner sums
-	for ( i = 1; i < hm_row - 1; i++)
+	for ( i = 0; i < hm_row ; i++)
 	{
-		for ( j = 1; j < hm_column - 1; j++)
+		for ( j = 0; j < hm_column; j++)
 		{
 			cnext = (1-hm_get_coeff(i,j))*sideCoef;
 			cdiag = 1-hm_get_coeff(i,j)-cnext;
@@ -76,7 +77,7 @@ void do_compute(const struct parameters* p, struct results *r)
 	}
 
 	//left side
-	for ( i = 1; i < hm_row - 1; i++)
+	for ( i = 0; i < hm_row; i++)
 	{
 
 		cnext = (1-hm_get_coeff(i,j))*sideCoef;
@@ -93,7 +94,7 @@ void do_compute(const struct parameters* p, struct results *r)
 
 	//  right side
 	j=hm_column-1;
-	for ( i = 1; i < hm_row - 1; i++)
+	for ( i = 0; i < hm_row; i++)
 	{
 		cnext = (1-hm_get_coeff(i,j))*sideCoef;
 		cdiag = 1-hm_get_coeff(i,j)-cnext;
@@ -117,7 +118,7 @@ void do_compute(const struct parameters* p, struct results *r)
 	r->tmax = 0;
 	r->tmin = p->io_tmax;
 	r->tavg = 0;
-	for(i = hm_column; i < hm_row * hm_column-hm_column; i++) {
+	for(i = 0; i < hm_row * hm_column; i++) {
 		//max
 		if (hm_data[i] > r->tmax) r->tmax = hm_data[i];
 		//min
@@ -138,6 +139,10 @@ void do_compute(const struct parameters* p, struct results *r)
 /**
  * hm_init_map
  *  returns 0 on succes
+ *
+ *  Note that the allocation is padded with two extra rows per map
+ *  Making x(-1) pointers valid as well as x(size).
+ *
  */
 int hm_init_map(struct parameters * p) {
 	//Set vars
@@ -146,30 +151,36 @@ int hm_init_map(struct parameters * p) {
 
 	//Init tmps
 	void * tmp1 = 0;
-	void * tmp2 = 0;
 	void * tmp3 = 0;
 
 	//Allocate alligned memory
-	int result1 = posix_memalign(&tmp1, 128/8, sizeof(double)*row*column+2*sizeof(double)*column);
-	int result2 = posix_memalign(&tmp2, 128/8, sizeof(double)*row*column+2*sizeof(double)*column);
-	int result3 = posix_memalign(&tmp3, 128/8, sizeof(double)*row*column);
+	int result1 = posix_memalign(&tmp1, 128/8, (sizeof(double)*(row+2)*(column))*2);
+	int result2 = posix_memalign(&tmp3, 128/8, sizeof(double)*row*column);
 
-	if (result1 || result2 || result2) {
+	if (result1 || result2) {
 		hm_data = 0;
-		if (HM_VERBOSE) printf("Error while allocating memory in hm_init_map: %i, %i, %i", result1, result2, result3);
+		if (HM_VERBOSE) printf("Error while allocating memory in hm_init_map: %i, %i", result1, result2);
 	}else {
-		hm_data     =   (double*) tmp1;
-		hm_prevData =   (double*) tmp2;
+		hm_data     =   (double*) (tmp1);
+		hm_prevData =   (double*) (tmp1+2*column+column*row); //skip two ghost rows and a map.
 		hm_coeff    =   (double*) tmp3;
 		hm_row     =   row+2;
 		hm_column  =   column;
 
+		//Copy data
+		// Heatmap
 		memcpy((void*)(hm_prevData+column), (void*)p->tinit, sizeof(double)*row*column);
+		// Prevdata
 		memcpy((void*)hm_coeff, (void*)p->conductivity, sizeof(double)*row*column);
 
 		//Ghost rows
-		memcpy((void*)hm_prevData, (void*)p->tinit, sizeof(double)*column);
+		// prevData
+		memcpy((void*)(hm_prevData), (void*)p->tinit, sizeof(double)*column);
 		memcpy((void*)(hm_prevData+(row+1)*column), (void*)(p->tinit+row*column-column), sizeof(double)*column);
+
+		// current map
+		memcpy((void*)(hm_data), (void*)p->tinit, sizeof(double)*column);
+		memcpy((void*)(hm_data+(row+1)*column), (void*)p->tinit, sizeof(double)*column);
 
 		//print test
 		begin_picture(666, column, row, p->io_tmin, p->io_tmax);
@@ -187,5 +198,5 @@ int hm_init_map(struct parameters * p) {
 
 	}
 
-	return result1 | result2 | result3;
+	return result1 | result2;
 }
