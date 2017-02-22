68888fc57ff233e9088de503238d589671936730
etteerr <e.j.diepgrond@gmail.com>
2017-02-22 12:50:36 +0100

    SSE implementation, cnext and cdiag OK. Something else isnt

--- heat-1.17/seq/compute.c
+++ heat-1.17/seq/compute.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <emmintrin.h>
-#include <time.h>
 #include "../src/compute.h"
 #include "../src/output.h"
 
-#define checkSanity 1
+#define checkSanity 0
 #define sanityPGM 0
 #define sideCoef 0.58578643762
 
@@ -22,45 +21,51 @@ size_t hm_column = 0;
 
 //Getters and setters
 // Inline for performance
-__attribute__((assume_aligned(16)))
-inline double hm_get(int row, int column) {
-	if (checkSanity) {
-		if (row > (int)hm_row || column > hm_column || row < -1 || column < 0)
-			printf("Invalid coördinate requested (%i,%i)\n", row, column);
-	}
-	return hm_prevData[hm_column * (row+1) + column];
-}
+#define hm_get(R,C) hm_prevData[hm_column * (R+1) + C]
+#define hm_geta(R,C) (hm_prevData +hm_column * (R+1) + C)
+//__attribute__((assume_aligned(16)))
+//inline double hm_get(int row, int column) {
+//	if (checkSanity) {
+//		if (row > (int)hm_row || column > hm_column || row < -1 || column < 0)
+//			printf("Invalid coördinate requested (%i,%i)\n", row, column);
+//	}
+//	return hm_prevData[hm_column * (row+1) + column];
+//}
 
-inline double * hm_get_p(int row, int column) {
-	return hm_prevData + hm_column * (row+1) + column;
-}
+#define hm_get_current(R,C) hm_data[hm_column * (R+1) + C]
+//__attribute__((assume_aligned(16)))
+//inline double hm_get_current(size_t  row, size_t  column) {
+//	if (checkSanity) {
+//		if (row > hm_row || column > hm_column || row < 0 || column < 0)
+//			printf("Invalid coördinate requested (current) (%i,%i)\n", row, column);
+//	}
+//	return hm_data[hm_column * (row+1) + column];
+//}
 
-__attribute__((assume_aligned(16)))
-inline double hm_get_current(size_t  row, size_t  column) {
-	if (checkSanity) {
-		if (row > hm_row || column > hm_column || row < 0 || column < 0)
-			printf("Invalid coördinate requested (current) (%i,%i)\n", row, column);
-	}
-	return hm_data[hm_column * (row+1) + column];
-}
+#define hm_set(R,C,V) hm_data[hm_column * (R+1) + C] = V
+//inline void hm_set(size_t row, size_t column, double val) {
+//	if (checkSanity) {
+//		if (row > hm_row || column > hm_column || row < 0 || column < 0)
+//			printf("Invalid coördinate set (%i,%i)\n", row, column);
+//	}
+//	*(hm_data + hm_column * (row+1) + column) = val;
+//}
 
-inline void hm_set(size_t row, size_t column, double val) {
-	if (checkSanity) {
-		if (row > hm_row || column > hm_column || row < 0 || column < 0)
-			printf("Invalid coördinate set (%i,%i)\n", row, column);
-	}
-	*(hm_data + hm_column * (row+1) + column) = val;
-}
-__attribute__((assume_aligned(16)))
-inline double hm_get_coeff(size_t row, size_t column) {
-	return hm_coeff[hm_column * row + column];
-}
+#define hm_get_coeff(R,C) hm_coeff[hm_column * R + C]
+#define hm_get_coeffa(R,C) (hm_coeff + hm_column * R + C)
+//__attribute__((assume_aligned(16)))
+//inline double hm_get_coeff(size_t row, size_t column) {
+//	return hm_coeff[hm_column * row + column];
+//}
 
-__attribute__((assume_aligned(16)))
-inline void hm_set_coeff(size_t row, size_t column, double val) {
-	hm_coeff[hm_column * row + column] = val;
-}
+#define hm_set_coeff(R,C,V) hm_coeff[hm_column * R + C] = V
+//__attribute__((assume_aligned(16)))
+//inline void hm_set_coeff(size_t row, size_t column, double val) {
+//	hm_coeff[hm_column * row + column] = val;
+//}
 
+//Inline is faster in this case
+//#define hm_swap { double * tmp = hm_data; hm_data = hm_prevData; hm_prevData = tmp; }
 inline void hm_swap() {
 	double * tmp = hm_data;
 	hm_data = hm_prevData;
@@ -88,6 +93,7 @@ void calcStatistics(struct parameters* p,struct results* r) {
     	r->tmax = 0;
     	r->tmin = 1e9;
     	r->tavg = 0;
+    	double sum = 0.0;
     	for(int i = 0; i < hm_row; i++)
     		for (int j =0; j < hm_column; j++) {
 				//max
@@ -95,190 +101,117 @@ void calcStatistics(struct parameters* p,struct results* r) {
 				//min
 				if (hm_get_current(i,j) < r->tmin) r->tmin = hm_get_current(i,j);
 				//maxdiff
-				if (abs(hm_get_current(i,j)-hm_get(i,j)) > r->maxdiff) r->maxdiff = abs(hm_get_current(i,j)-hm_get(i,j));
+				if (fabs(hm_get_current(i,j)-hm_get(i,j)) > r->maxdiff) r->maxdiff = fabs(hm_get_current(i,j)-hm_get(i,j));
 				//searchme
-				r->tavg += hm_get_current(i,j)/(hm_column*hm_row);
+				sum += hm_get_current(i,j);
     		}
+
+    	r->tavg += sum/(hm_column*hm_row);
 }
-const int a = _MM_SHUFFLE2(1,0); //Shuffle operator 'a' [0 (0][1) 1]=> [0 1]
-const int b = _MM_SHUFFLE2(0,0); //Shuffle operator 'b' [0 (0][1) 1]=> [1 0]
-#define _ESWAPA(A,B,OP1, OP2){ \
-	__v2df __ESWAPA_TMP; \
-	__ESWAPA_TMP = _mm_shuffle_pd(A,B,OP1); \
-	B   = _mm_shuffle_pd(A,B,OP2); \
-	A   = __ESWAPA_TMP; \
-	}
 
 void do_compute(const struct parameters* p, struct results *r)
 {
-	size_t i, j;
-	double cdiag, cnext;
-	double * p2v2; //pointer 2 vector 2
-	__v2df top2, bot2, mid2, mid1, top1, bot1, next1, res; // 1+ 6
-
-	// Calculate the inner sums
-	for ( i = 0; i < hm_row ; i++)
-	{
-		for ( j = 1; j < hm_column-1-1; j+=2) //Extra -1 for prevent j overflow in heatmap
+		//Intresting way of declaring privates
+		double cdiag, cnext;
+		unsigned int i, j;
+		// Calculate the inner sums
+		for ( i = 0; i < hm_row ; i++)
 		{
-			//Init
-			next1 = _mm_set1_pd(0.0);
 
-			//Load + shuffle top
-			top2 = _mm_load_pd(hm_get_p(i-1, j-1));
-			top1 = _mm_load_pd(hm_get_p(i-1, j+1));
-			_ESWAPA(top2, top1, a, b); //top2 is diag top, top1 is middle
-
-			//Load + shuffle bot
-			bot2 = _mm_load_pd(hm_get_p(i+1, j-1));
-			bot1 = _mm_load_pd(hm_get_p(i+1, j+1));
-			printf("%.5f %.5f\n", ((double *)&bot2)[0], ((double *)&bot2)[1]);
-			printf("%.5f %.5f\n", ((double *)&bot1)[0], ((double *)&bot1)[1]);
-			_ESWAPA(bot2, bot1, a, b); //bot2 is diag bot, bot1 is middle
-			printf("%.5f %.5f\n", ((double *)&bot2)[0], ((double *)&bot2)[1]);
-			printf("%.5f %.5f\n", ((double *)&bot1)[0], ((double *)&bot1)[1]);
-			//shuffle  bot1&top1
-			_ESWAPA(top1, bot1, a, b); //Shuffles 0 x 1 x to 01xx, bot1 now next side info
-
-			//Load  + shuffle mid
-			mid2 = _mm_load_pd(hm_get_p(i+0, j-1));
-			mid1 = _mm_load_pd(hm_get_p(i+0, j+1));
-			_ESWAPA(mid2, mid1, a, b); //mid2 is next mid, mid1 is middle (+ mid next)
-
-			//Shuffle
-
-			_ESWAPA(mid1, next1,a,b); //replace mid1[1] with 0 and set next[0] to the second
-			//((double *)&mid1)[1] = 0.0;
-
-			//Calc modifiers
-			cnext = (1-hm_get_coeff(i,j))*sideCoef;
-			cdiag = 1-hm_get_coeff(i,j)-cnext;
-			cnext /= 4;
-			cdiag /= 4;
-
-			//Calc with weight and sum
-			// Note that filling xmmX is left to compiler  (*= instead of _mm_mul_pd)
-			res = (top2 * cdiag) + (bot2 * cdiag) + (mid2 * cnext) + (top1 * cnext) + (mid1 * hm_get_coeff(i,j)); //mid1[1] == 0
-
-			//sum final
-			p2v2 = (double*)&res; //reinterpret as double[2]
-			p2v2[0] += p2v2[1];    //Add double[1] to double[0]
-
-			//Save
-			hm_set(i,j,p2v2[0]); //Store double[0]
-
-			//Next cell
-
-			//Shuffle
-			_ESWAPA(mid1, next1, a, b);
-
-			//Calc modifiers
-			cnext = (1-hm_get_coeff(i,j+1))*sideCoef;
-			cdiag = 1-hm_get_coeff(i,j+1)-cnext;
-			cnext /= 4;
-			cdiag /= 4;
-
-			//calculate
-			top2 *= cnext;
-			mid1 *= cnext;
-			top1 *= cdiag;
-			bot1 *= cdiag;
-			mid2 *= hm_get_coeff(i,j+1);
-
-			//sum
-			top2 += mid1 + top2 + bot1;
-			p2v2 = (double *)&mid2; //Our mid value is in mid2[1]
-			top2 += p2v2[1];
-			p2v2 = (double *)&top2; //Result is stored in top2[0:1]
-			p2v2[0] += p2v2[1];
-
-			//store
-			hm_set(i,j+1, p2v2[0]);
-
-//			hm_set(i,j,
-//					//SUm neighbours (direct)
-//					((hm_get(i-1,j) + hm_get(i+1,j) + hm_get(i,j-1) + hm_get(i,j+1))/4.0)*cnext +
-//					//Sum diag
-//					((hm_get(i-1,j-1) + hm_get(i+1,j+1) + hm_get(i+1,j-1) + hm_get(i-1,j+1))/4.0)*cdiag +
-//					//Add current
-//					hm_get(i,j) * hm_get_coeff(i,j)
-//			);
-
-			if (checkSanity) {
-				if (hm_get(i,j) != 1.0 || hm_get_current(i,j) != 1.0) {
-					printf("(%i,%i) %.5f -> %.5f\n",i,j, hm_get(i,j), hm_get_current(i,j));fflush(stdout);
-					printf("(%i,%i) %.5f -> %.5f\n",i,j+1, hm_get(i,j+1), hm_get_current(i,j+1));fflush(stdout);
-					sleep(1);
-				}
-			}
-		}
-
-		//Uneven case
-		if (hm_column % 2 == 1) {
-
-			//set j
-			j = hm_column - 2;
-
-			//Calc modifiers
-			cnext = (1-hm_get_coeff(i,j+1))*sideCoef;
-			cdiag = 1-hm_get_coeff(i,j+1)-cnext;
-			//Calc
-			hm_set(i,j,
+			//left
+			cnext = (1-hm_get_coeff(i,0))*sideCoef;
+			cdiag = 1-hm_get_coeff(i,0)-cnext;
+			hm_set(i,0,
 					//SUm neighbours (direct)
-					((hm_get(i-1,j) + hm_get(i+1,j) + hm_get(i,j-1) + hm_get(i,j+1))/4.0)*cnext +
+					((hm_get(i-1,0) + hm_get(i+1,0) + hm_get(i,hm_column-1) + hm_get(i,1))*.25)*cnext +
 					//Sum diag
-					((hm_get(i-1,j-1) + hm_get(i+1,j+1) + hm_get(i+1,j-1) + hm_get(i-1,j+1))/4.0)*cdiag +
+					((hm_get(i-1,hm_column-1) + hm_get(i+1,1) + hm_get(i+1,hm_column-1) + hm_get(i-1,1))*.25)*cdiag +
 					//Add current
-					hm_get(i,j) * hm_get_coeff(i,j)
+					hm_get(i,0) * hm_get_coeff(i,0)
+			);
+
+			//Middle
+			for ( j = 1; j < hm_column-1; j+=2)
+			{
+				//Calculate both cnext and cdiag for i,j and i,j+1
+				__m128d cnextd, cdiagd;
+				cdiagd = cnextd = 1 - _mm_loadu_pd(hm_get_coeffa(i,j));
+				cnextd *= sideCoef;
+				cdiagd -= cnext;
+				cnextd *= .25;
+				cdiagd *= .25;
+
+				//Assigned calculated cnext & cdiag
+				cnext = (double)cnextd[0];
+				cdiag = (double)cdiagd[0];
+
+				//i,j
+				__m128d top2, top1, mid2, mid1, bot2, bot1, diag1, diag2, next1, next2; //10/16 vars
+
+				//Load stage 1
+				top2 = _mm_load_pd(hm_geta(i-1,j-1));//upper  left + 1
+				top1 = _mm_load_pd(hm_geta(i-1,j+1));//upper  right + 1
+				mid2 = _mm_load_pd(hm_geta(i  ,j-1));//middle left + middle
+				mid1 = _mm_load_pd(hm_geta(i  ,j+1));//middle right + 1
+				bot2 = _mm_load_pd(hm_geta(i+1,j-1));//bot    left + 1
+				bot1 = _mm_load_pd(hm_geta(i+1,j+1));//bot    right + 1
+
+				//Sort
+				//Note: _MM_SHUFFLE2(SecondVar, firstVar)
+				diag1 = _mm_shuffle_pd(top2, top1, _MM_SHUFFLE2(0,0)); //First of top2 and first of top1
+				diag2 = _mm_shuffle_pd(bot2, bot1, _MM_SHUFFLE2(0,0)); //First of bot2 and first of bot1
+				next2 = _mm_shuffle_pd(mid2, mid1, _MM_SHUFFLE2(0,0)); //First and first
+				next1 = _mm_shuffle_pd(top2, bot2, _MM_SHUFFLE2(1,1)); //second of top2 and bot2
+
+				//Calculate weighted
+				diag1 *= cdiag;
+				diag2 *= cdiag;
+				next1 *= cnext;
+				next2 *= cnext;
+
+				//Sum
+				diag1 += diag2 + next1 + next2;
+				hm_set(i,j,(double)diag1[0] + (double)diag1[1]);
+
+				//([-1 (0][1)2])[3 4]
+
+				//Assigned calculated cnext & cdiag
+				cnext = (double)cnextd[1];
+				cdiag = (double)cdiagd[1];
+
+				//Sort
+				diag1 = _mm_shuffle_pd(top1, top2, _MM_SHUFFLE2(1,1));
+				diag2 = _mm_shuffle_pd(bot1, bot2, _MM_SHUFFLE2(1,1));
+				next1 = _mm_shuffle_pd(mid1, mid2, _MM_SHUFFLE2(1,1));
+				next2 = _mm_shuffle_pd(top1, bot1, _MM_SHUFFLE2(0,0));
+
+				//Calculate weighted
+				diag1 *= cdiag;
+				diag2 *= cdiag;
+				next1 *= cnext;
+				next2 *= cnext;
+
+				//Sum
+				diag1 += diag2 + next1 + next2;
+				hm_set(i,j+1,(double)diag1[0] + (double)diag1[1]);
+
+			}
+
+			//right
+			cnext = (1-hm_get_coeff(i,(hm_column-1)))*sideCoef;
+			cdiag = 1-hm_get_coeff(i,(hm_column-1))-cnext;
+			hm_set(i,(hm_column-1),
+					//SUm neighbours (direct)
+					((hm_get(i-1,(hm_column-1)) + hm_get(i+1,(hm_column-1)) + hm_get(i,(hm_column-1)-1) + hm_get(i,0))*.25)*cnext +
+					//Sum diag
+					((hm_get(i-1,(hm_column-1)-1) + hm_get(i+1,0) + hm_get(i+1,(hm_column-1)-1) + hm_get(i-1,0))*.25)*cdiag +
+					//Add current
+					hm_get(i,(hm_column-1)) * hm_get_coeff(i,(hm_column-1))
 			);
 		}
-	}
 
-
-	//left side
-	for ( i = 0; i < hm_row; i++)
-	{
-
-		cnext = (1-hm_get_coeff(i,j))*sideCoef;
-		cdiag = 1-hm_get_coeff(i,j)-cnext;
-		hm_set(i,j,
-				//SUm neighbours (direct)
-				((hm_get(i-1,j) + hm_get(i+1,j) + hm_get(i,hm_column-1) + hm_get(i,j+1))/4.0)*cnext +
-				//Sum diag
-				((hm_get(i-1,hm_column-1) + hm_get(i+1,j+1) + hm_get(i+1,hm_column-1) + hm_get(i-1,j+1))/4.0)*cdiag +
-				//Add current
-				hm_get(i,j) * hm_get_coeff(i,j)
-		);
-
-		if (checkSanity) {
-			if (hm_get(i,j) != 1.0 || hm_get_current(i,j) != 1.0) {
-				printf("(%i,%i) %.5f -> %.5f\n",i,j, hm_get(i,j), hm_get_current(i,j));fflush(stdout);
-			}
-		}
-	}
-
-	//  right side
-	j=hm_column-1;
-	for ( i = 0; i < hm_row; i++)
-	{
-		cnext = (1-hm_get_coeff(i,j))*sideCoef;
-		cdiag = 1-hm_get_coeff(i,j)-cnext;
-		hm_set(i,j,
-				//SUm neighbours (direct)
-				((hm_get(i-1,j) + hm_get(i+1,j) + hm_get(i,j-1) + hm_get(i,0))/4.0)*cnext +
-				//Sum diag
-				((hm_get(i-1,j-1) + hm_get(i+1,0) + hm_get(i+1,j-1) + hm_get(i-1,0))/4.0)*cdiag +
-				//Add current
-				hm_get(i,j) * hm_get_coeff(i,j)
-		);
-	}
-
-	if (checkSanity && sanityPGM) renderImageMemory(r->niter, -100.0, +100.0);
-
-	hm_swap();
-
-	// add inter
-	r->niter++;
+		r->niter++;
+		hm_swap();
 
 }
 
@@ -318,7 +251,7 @@ int hm_init_map(struct parameters * p) {
 		// Heatmap
 		memcpy((void*)(hm_prevData+column), (void*)p->tinit, sizeof(double)*row*column);
 		memcpy((void*)(hm_data+column), (void*)p->tinit, sizeof(double)*row*column); // Fixes strange problems
-		// coeff
+		// Prevdata
 		memcpy((void*)hm_coeff, (void*)p->conductivity, sizeof(double)*row*column);
 
 		//Ghost rows
@@ -343,19 +276,11 @@ int hm_init_map(struct parameters * p) {
 					draw_point(i,j,hm_get(i,j));
 
 		end_picture();
-		begin_picture(670, column, row, p->io_tmin, p->io_tmax);
-			for (int i = 1; i < row+1; i++)
-				for(int j = 0; j < column; j++)
-					draw_point(i,j,hm_get_coeff(i,j));
-
-		end_picture();
-
-		renderImageMemory(668, p->io_tmin, p->io_tmax);
 
 		if (checkSanity) {
 	//		Test map
 			for (int i = 0; i < (column*(row+2))*2; i++)
-				hm_data[i] = i;// ((double)rand()/RAND_MAX)*(double)rand();
+				hm_data[i] = 1.0;// ((double)rand()/RAND_MAX)*(double)rand();
 		}
 	}//end of allocate succes if
 	return result1 | result2;
