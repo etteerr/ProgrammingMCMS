c3ca3250d50019e825eb2188aea624034a7f4ff9
etteerr <e.j.diepgrond@gmail.com>
2017-03-01 10:36:02 +0100

    Added result write to csv in main

--- heat-1.17/src/main.c
+++ heat-1.17/src/main.c
@@ -1,8 +1,17 @@
 #include "compute.h"
 #include <sys/time.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #define convThreshold 1.0e-3
+
+#define FPOPS_PER_POINT_PER_ITERATION (                 \
+        1     /* current point 1 mul */ +               \
+        3 + 1 /* direct neighbors 3 adds + 1 mul */ +   \
+        3 + 1 /* diagonal neighbors 3 adds + 1 mul */ + \
+        2     /* final add */ +                         \
+        1     /* difference old/new */                  \
+        )
 
 int main(int argc, char **argv)
 {
@@ -24,7 +33,7 @@ int main(int argc, char **argv)
     //wait a sec or 2
     sleep(2);
 
-    printf("%i, %i\n", p.maxiter, r.niter);
+    printf("%lu, %lu\n", p.maxiter, r.niter);
 
     //Main loop
     int j;
@@ -48,6 +57,32 @@ int main(int argc, char **argv)
     	//check stop condition
     	if (r.maxdiff < p.threshold) break;
     }
+
+    //Save results appending to file
+    FILE *fp;
+    fp = fopen("heatResult.csv","a");
+    printf("%li", ftell(fp));
+
+    //Check filestream
+    if (fp==0) {
+    	fprintf(stderr, "Failed to write results to file.\n");
+    	return 1;
+    }
+
+    //If its a new file, print header
+    if (ftell(fp)==0) {
+    	fprintf(fp, "File;Rows;Columns; Iterations; FLOP/s;\n");
+    }
+
+    //Print data
+    fprintf(fp,"%s;%lu;%lu;%lu;% .6e\n", p.filename, p.N, p.M, r.niter,
+    		(double) p.N * (double)p.M *
+			(double)(r.niter * FPOPS_PER_POINT_PER_ITERATION +
+			(double) r.niter / p.period) / r.time);
+
+    fflush(fp);
+    fclose(fp);
+
 
     return 0;
 }
